53f1830c733274348e0c2b03140abb97f1d229a3
Moinuddin-kader <you@example.com>
2022-05-07 15:27:27 +0600

    Added manage user endpoints

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,12 +16,23 @@ class UserSerializer(serializers.ModelSerializer):
         '''Create and return a new user with encrypted password'''
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        '''Updated a user, setting the password correctly'''
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     '''Serializer for the user authentication object'''
     email = serializers.CharField()
     password = serializers.CharField(
-        style={'input-type':  'password'},
+        style={'input_type':'password'},
         trim_whitespace=False
     )
 
